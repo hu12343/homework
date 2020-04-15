6afc4a8b59be72e874ff454b982951a74fe47610
Administrator <1214310681@qq.com>
2020-04-15 15:31:01 +0800

    test

---
+++ src/main/java/test/OrderEntity.java
@@ -0,0 +1,86 @@
+package test;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order", schema = "hu", catalog = "")
+public class OrderEntity {
+    private int id;
+    private int cid;
+    private Double price;
+    private String address;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "cid")
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Basic
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderEntity that = (OrderEntity) o;
+
+        if (id != that.id) return false;
+        if (cid != that.cid) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + cid;
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderEntity{" +
+                "id=" + id +
+                ", cid=" + cid +
+                ", price=" + price +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
